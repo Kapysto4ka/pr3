3d4f471f7a4c9962ad7c5431b0c7aa2295b0fdb4
Kapysto4ka <anatolii.bilohan-ki2001@kep.nung.edu.ua>
2023-10-31 23:53:22 +0200

    added focus on input

--- script.js
+++ script.js
@@ -3,12 +3,8 @@ const addButton = document.querySelector('input[type="submit"]');
 addButton.addEventListener("click", function(event) {
   const input = document.querySelector('input[type="text"]').value;
   const table = document.querySelector("table");
-   event.preventDefault();
-
   const ul = document.createElement("ul");
-
   const li = document.createElement("li");
-
 
   li.textContent = input;
   if(li.textContent.trim() === ""){
@@ -24,11 +20,13 @@ addButton.addEventListener("click", function(event) {
 
   edit.addEventListener("click", function(){
     let text =  button.previousSibling.nodeValue
-    button.previousSibling.nodeValue = "";
     const changeItem = document.createElement("input");
-    changeItem.value = button.previousSibling.nodeValue;
     const saveButton = document.createElement("button");
     const cancelButton = document.createElement("button");
+
+    changeItem.value = text;
+    button.previousSibling.nodeValue = "";
+
     cancelButton.addEventListener("click",function (){
       saveButton.remove();
       cancelButton.remove();
@@ -52,12 +50,15 @@ addButton.addEventListener("click", function(event) {
 
     saveButton.textContent = "Save"
     cancelButton.textContent = "Cancel"
+
     li.appendChild(changeItem);
     li.appendChild(saveButton);
     li.appendChild(cancelButton);
+    changeItem.focus()
     button.classList.toggle('hide');
     edit.classList.toggle('hide');
    });
+
   edit.textContent = "Edit";
   button.textContent = "Delete";
   li.appendChild(button);
